bcc0ce1d5b1d7e5ce0eae2cbf5a9da9e243f5a06
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-24 16:17:58 +0100

    questions 1 ok: affichage nombre de mots, questions 2 ok: affichage nombre de mots avec 15 caractères

---
+++ dictionnaire/index.php
@@ -0,0 +1,14 @@
+<?php
+$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
+$dico = explode("\n", $string);
+// echo "Le dictionnaire contient ".count($dico)." "."mots";
+$compteur = 0;
+foreach($dico as $value){
+	$wordLength = strlen($value);
+	if($wordLength === 15){
+		$compteur++;
+	};
+}
+echo "le dico contient"."\n".$compteur."\n"."mots de 15 caractères";
+
+
